5ba546313676df1f79ac4568833967f3f557786b
Gourav Beura <bueragourav@gmail.com>
2026-04-25 19:22:21 -0500

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+    
+    def longest_common_prefix(self, word:str, prefix_len:int)->int:
+        node = self.root
+
+        for i in range(min(len(word),prefix_len)):
+            if word[i] not in node.children:
+                return i
+            node = node.children[word[i]]
+        return min(len(word), prefix_len)
+    
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        
+        mini = 0
+        for i in range(1, len(strs)):
+            if len(strs[mini]) > len(strs[i]):
+                mini = i
+            
+        trie = Trie()
+        trie.insert(strs[mini])
+        prefix_len = len(strs[mini])
+        for i in range(len(strs)):
+            prefix_len = trie.longest_common_prefix(strs[i], prefix_len)
+        
+        return strs[0][:prefix_len]
+
+
+
